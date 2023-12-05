fc649d8f00794080d5122675fa04de451d2d705e
Your Name Here <fayyyufei@gmail.com>
2023-12-05 08:29:48 -0800

    Deploy React app to GitHub Pages

---
+++ precache-manifest.96f4c86bbd1288fd0c9a2682c8d7b9c4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "031e7b1738af86ad7a6f63b18519272c",
+    "url": "/MontyHallGame/index.html"
+  },
+  {
+    "revision": "55b03b047aaf233aa5cb",
+    "url": "/MontyHallGame/static/css/2.592b17de.chunk.css"
+  },
+  {
+    "revision": "d4350b7087d970148c23",
+    "url": "/MontyHallGame/static/css/main.5797e2ee.chunk.css"
+  },
+  {
+    "revision": "55b03b047aaf233aa5cb",
+    "url": "/MontyHallGame/static/js/2.d62783cd.chunk.js"
+  },
+  {
+    "revision": "075ee0fe56affc8020645735366f8cbd",
+    "url": "/MontyHallGame/static/js/2.d62783cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09205dcad99e78605eae",
+    "url": "/MontyHallGame/static/js/3.81ecac3d.chunk.js"
+  },
+  {
+    "revision": "d4350b7087d970148c23",
+    "url": "/MontyHallGame/static/js/main.20697389.chunk.js"
+  },
+  {
+    "revision": "4b3df57446d44f12eb4d",
+    "url": "/MontyHallGame/static/js/runtime-main.c793d043.js"
+  },
+  {
+    "revision": "db2ece12e69f3d6002f820a677d365f3",
+    "url": "/MontyHallGame/static/media/bayes.7e7e3282.jpg"
+  },
+  {
+    "revision": "b4e092eac12fd5f779e8008718e114cd",
+    "url": "/MontyHallGame/static/media/car_tr.e3b10ac6.png"
+  },
+  {
+    "revision": "29c218154a8b55930fa0d6271b3e6971",
+    "url": "/MontyHallGame/static/media/goat_tr.aa11c8c2.png"
+  }
+]);
